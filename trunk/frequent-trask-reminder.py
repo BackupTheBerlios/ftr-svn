8d587717d40e2ee6e7abb0182b19c92d60c3aa99
gradha <gradha@c375e13b-eee2-0310-87b3-a263ce5279dc>
2004-09-01 19:35:24 +0000

    Corred value for task/id tag. Removed task/notes tag. Added string_to_seconds(). Loading of configuration file and printing the data of all tasks.

--- trunk/frequent-trask-reminder.py
+++ trunk/frequent-trask-reminder.py
@@ -11,6 +11,7 @@ import time
 
 # Some globals.
 HUMAN_VERSION = "0.1.0"
+DAY_IN_SECONDS = float(24 * 60 * 60)
 
 
 def get_today():
@@ -29,21 +30,42 @@ def create_empty_configuration_file(file_name):
     # Create a default empty task.
     tasklist = SubElement(root, "tasklist")
     task = SubElement(tasklist, "task")
-    SubElement(task, "id").text = 0
+    SubElement(task, "id").text = "0"
     SubElement(task, "name").text = "Default name"
-    SubElement(task, "notes").text = "Some random notes you can modify."
     SubElement(task, "starting-day").text = get_today()
     
     # Finally write the xml file.
     ElementTree(root).write(file_name, encoding="latin1")
     print "Created empty configuration file '%s'." % file_name
 
+
+def string_to_seconds(text_string):
+    """Returns a string in format YYYY-MM-DD into seconds since epoch."""
+    return time.mktime(time.strptime(text_string, "%Y-%m-%d"))
+    
     
 def main():
     """Entry point of the binary."""
     file_name = os.path.expanduser("~/.frequent-task-reminderrc")
     if not os.path.isfile(file_name):
         create_empty_configuration_file(file_name)
+
+    # Read the file.
+    data = ElementTree(file = file_name)
+
+    # Print how many tasks there are.
+    print "Tracking %d task(s)." % len(data.getiterator("task"))
+    for node in data.getiterator("task"):
+        print "---"
+        print "Task '%s'" % node.find("name").text
+        
+        date_in_string = node.find("starting-day").text
+        date_in_seconds = string_to_seconds(date_in_string)
+        current = time.time()
+        assert current > date_in_seconds
+        days = int((current - date_in_seconds) / DAY_IN_SECONDS)
+        
+        print "Started on %s, %d days ago" % (date_in_string, days)
         
 
 if __name__ == "__main__":
