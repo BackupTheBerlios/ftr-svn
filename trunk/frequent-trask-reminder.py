9c65e1ae9f7cfb89c14ce131440b88d612a562fe
gradha <gradha@c375e13b-eee2-0310-87b3-a263ce5279dc>
2004-09-01 21:37:27 +0000

    Added license text, some metadata and basic command line parameter processor.

--- trunk/frequent-trask-reminder.py
+++ trunk/frequent-trask-reminder.py
@@ -1,17 +1,95 @@
 #!/usr/bin/env python
 # -*- mode:Python; tab-width: 4 -*-
+"""Simple daily task reminder.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+"""
+
+__author__ = "Grzegorz Adam Hankiewicz"
+__date__ = "$Date$"
+__version__ = "$Rev$"
+__email__ = "gradha@users.sourceforge.net"
+__credits__ = ""
+
 
 # Modules to be imported.
 from elementtree.ElementTree import Element
 from elementtree.ElementTree import ElementTree
 from elementtree.ElementTree import SubElement
 import os
+import sys
 import time
 
 
 # Some globals.
 HUMAN_VERSION = "0.1.0"
 DAY_IN_SECONDS = float(24 * 60 * 60)
+
+
+def usage_information(exit_code = 0, binary_name = "frequent-task-reminder.py"):
+    """Prints usage information and terminates execution."""
+    print """Usage: %s [-hv -i xxx -o xxx]
+
+-h, --help
+   Print this help screen.
+-v, --version
+   Print version number and exit.
+
+
+Usage examples:
+ %s
+""" % (binary_name, binary_name)
+    sys.exit(exit_code)
+
+
+def process_command_line(argv = None):
+   """Extracts from argv the options and returns them in a tuple.
+
+   Errrfff... update this to something usefull...
+
+   This function is a command line wrapper against main_process,
+   it returns a tuple which you can `apply' calling main_process. If
+   something in the command line is missing, the program will exit
+   with a hopefully helpfull message.
+
+   args should be a list with the full command line. If it is None
+   or empty, the arguments will be extracted from sys.argv. The
+   correct format of the accepted command line is documented by
+   usage_information.
+   """
+   import getopt
+   if not argv:
+      argv = sys.argv
+
+   short_list = "hv"
+   long_list = ["help", "version"]
+
+   try:
+      opts, args = getopt.getopt(argv[1:], short_list, long_list)
+   except getopt.error, msg:
+      print "Error processing command line: %s\n" % msg
+      usage_information(2)
+
+   for option, value in opts:
+      if option in ("-h", "--help"):
+         usage_information()
+      elif option in ("-v", "--version"):
+         print HUMAN_VERSION
+         sys.exit(0)
+
+   return None
 
 
 def get_today():
@@ -25,7 +103,7 @@ def create_empty_configuration_file(file_name):
     root = Element("frequent-task-reminder")
     
     # Create the configuration element.
-    config = SubElement(root, "configuration")
+    SubElement(root, "configuration")
     
     # Create a default empty task.
     tasklist = SubElement(root, "tasklist")
@@ -60,29 +138,30 @@ def list_tasks(tree_root):
     """Lists all the tasks from an xml tree."""
     # Calculate the number of days used on each task.
     units_done = find_units_done(tree_root)
+
+    current_time = time.time()
     
     # Print how many tasks there are.
     print "Tracking %d task(s)." % len(tree_root.getiterator("task"))
     for node in tree_root.getiterator("task"):
         print "---"
-        id = node.find("id").text
-        print "Task %s" % id
+        task_id = node.find("id").text
+        print "Task %s" % task_id
         print "Name '%s'" % node.find("name").text
         
         date_in_string = node.find("starting-day").text
         date_in_seconds = string_to_seconds(date_in_string)
-        current = time.time()
-        assert current > date_in_seconds
-        days = int((current - date_in_seconds) / DAY_IN_SECONDS)
-        done = units_done.get(id, 0)
+        assert current_time > date_in_seconds
+        days = int((current_time - date_in_seconds) / DAY_IN_SECONDS)
+        done = units_done.get(task_id, 0)
         
         print "Started on %s, %d days ago" % (date_in_string, days)
         print "Work units done %d, remaining to be done %d" % (done,
             days + 1 - done)
 
 
-def main():
-    """Entry point of the binary."""
+def main_process():
+    """Does the main task of running the program."""
     file_name = os.path.expanduser("~/.frequent-task-reminderrc")
     if not os.path.isfile(file_name):
         create_empty_configuration_file(file_name)
@@ -94,4 +173,5 @@ def main():
         
 
 if __name__ == "__main__":
-    main()
+    process_command_line()
+    main_process()
