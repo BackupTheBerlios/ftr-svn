e8adc1cb838aebf0b3bb8893083ba0087963e656
gradha <gradha@c375e13b-eee2-0310-87b3-a263ce5279dc>
2004-09-01 08:24:41 +0000

    Using custom date. Now starting-day is a child of task nodes.

--- trunk/frequent-trask-reminder.py
+++ trunk/frequent-trask-reminder.py
@@ -13,6 +13,12 @@ import time
 HUMAN_VERSION = "0.1.0"
 
 
+def get_today():
+    """Returns the string year-mm-dd using today's date."""
+    t = time.localtime()
+    return "%04d-%02d-%02d" % (t[0], t[1], t[2])
+
+
 def create_empty_configuration_file(file_name):
     """Creates a default empty file with today's date as starting day."""
     root = Element("frequent-task-reminder")
@@ -20,16 +26,13 @@ def create_empty_configuration_file(file_name):
     # Create the configuration element.
     config = SubElement(root, "configuration")
     
-    # Set the first day to track as today.
-    start = SubElement(config, "starting-day")
-    start.text = time.asctime()
-    
     # Create a default empty task.
     tasklist = SubElement(root, "tasklist")
     task = SubElement(tasklist, "task")
     SubElement(task, "id").text = 0
     SubElement(task, "name").text = "Default name"
     SubElement(task, "notes").text = "Some random notes you can modify."
+    SubElement(task, "starting-day").text = get_today()
     
     # Finally write the xml file.
     ElementTree(root).write(file_name, encoding="latin1")
