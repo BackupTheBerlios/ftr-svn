71f62a997028018825d77b447c49e3bb321ea4fc
gradha <gradha@c375e13b-eee2-0310-87b3-a263ce5279dc>
2005-06-17 23:07:56 +0000

    Using berlios email address instead of sourceforge's.

--- trunk/frequent-task-reminder.py
+++ trunk/frequent-task-reminder.py
@@ -20,7 +20,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 __author__ = "Grzegorz Adam Hankiewicz"
 __date__ =  "$Date$"
 __version__ = "$Rev$"
-__email__ = "gradha@users.sourceforge.net"
+__email__ = "gradha@users.berlios.de"
 __credits__ = ""
 
 
